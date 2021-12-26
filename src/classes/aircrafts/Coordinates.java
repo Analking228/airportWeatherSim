825eab778863b5567dc41a7f39525d8688da834c
Павел <penetrator3000@MacBook-Pro-Pavel.local>
2021-12-26 12:42:45 +0300

    coords fix

--- src/classes/aircrafts/Coordinates.java
+++ src/classes/aircrafts/Coordinates.java
@@ -1,25 +1,25 @@
 package classes.aircrafts;
 
-public class        Coordinates {
-    private int     longitude;
-    private int     latitude;
-    private int     height;
+public class            Coordinates {
+    private final int   longitude;
+    private final int   latitude;
+    private final int   height;
 
-    public Coordinates(int longitude, int latitude, int height) {
+    public              Coordinates(int longitude, int latitude, int height) {
         this.longitude = longitude;
         this.latitude = latitude;
-        this.height = height;
+        this.height = Math.min(height, 100);
     }
 
-    public int      getLongitude() {
+    public int          getLongitude() {
         return longitude;
     }
 
-    public int      getLatitude() {
+    public int          getLatitude() {
         return latitude;
     }
 
-    public int      getHeight() {
+    public int          getHeight() {
         return height;
     }
 }
